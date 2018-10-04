36d279f18600e9ce979fc3de642fa468c346d62f
Prannav Gupta <prannavgupta23@outlook.com>
2018-10-03 20:01:31 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,121 @@
 import React, { Component } from "react";
+
+
+
 class App extends Component {
+
   state = {
+
     newTodo: "",
+
     todos: [
+
       "Buy tea",
+
       "Buy boba",
+
       "Boil water",
+
       "Add boba",
+
       "Drain water",
+
       "Add tea"
+
     ]
+
   };
 
+
+
+  handleDelete = event => {
+
+    var newTodoList = [...this.state.todos]; 
+
+    var index = newTodoList.indexOf(event.target.value);
+
+    newTodoList.splice(index, 1);
+
+    this.setState({todos: newTodoList});
+
+  };
+
+
+
+  handleChange = event => {
+
+    this.setState({ newTodo: event.target.value });
+
+  };
+
+
+
+  handleSubmit = event => {
+
+    event.preventDefault();
+
+    this.setState({
+
+      todos: [...this.state.todos, this.state.newTodo],
+
+      newTodo: ""
+
+    });
+
+  };
+
+
+
   render() {
+
     return (
+
       <div>
+
         <h1> How to make boba at home</h1>
+
         {this.state.todos.map(todo => (
-          <li key={todo}> {todo}</li>
+
+          <div>
+
+            <li key={todo}>
+
+              {todo + " "}
+
+              <button  value={todo} onClick={this.handleDelete}>X</button>
+
+            </li>
+
+          </div>
+
         ))}
+
+
+
+        <form onSubmit={this.handleSubmit}>
+
+          <input
+
+            type="text"
+
+            value={this.state.newTodo}
+
+            onChange={this.handleChange}
+
+          />
+
+          <button onClick={this.handleSubmit}> + </button>
+
+        </form>
+
       </div>
+
     );
+
   }
+
 }
+
+
 
 export default App;
